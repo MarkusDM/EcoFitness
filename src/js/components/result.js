fbccaf961604e5a0d503577b2025d0b206c32474
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2024-05-17 13:01:06 +0400

    fix results block, add grade-oda and footer blocks

--- src/js/components/result.js
+++ src/js/components/result.js
@@ -4,7 +4,7 @@ import 'swiper/css/bundle';
 function result() {
 
 
-  const swiperMmainBanner = new Swiper('.result__swiper', {
+  const swiperResutlPhoto = new Swiper('.result__swiper', {
     slidesPerView: 1,
     effect: 'fade',
     allowTouchMove: false,
@@ -21,7 +21,24 @@ function result() {
         prevEl: '.result__swiper-btn--prev',
     },
   });
-
+  const swiperTextBox = new Swiper('.result__info-swiper', {
+    slidesPerView: 1,
+    autoHeight: true,
+    effect: 'fade',
+    allowTouchMove: false,
+    fadeEffect: {
+      crossFade: true
+    },
+    speed: 1000,
+    breakpoints: {
+      768: {
+      }
+    },
+    navigation: {
+        nextEl: '.result__swiper-btn--next',
+        prevEl: '.result__swiper-btn--prev',
+    },
+  });
 }
 
 export default result;
